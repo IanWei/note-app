92fda3d92d7813d4616fda0573ba3a1a272e15be
IanWei <ikaeuu@gmail.com>
2018-07-29 12:18:44 +0000

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Nav />  
+        <List />
+        <Note />
       </div>
     );
   }
